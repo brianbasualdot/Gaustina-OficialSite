6b0fc4271e26ddeadd7802855deb31994a26840f
brianbasualdot <brianbasualdot@gmail.com>
2026-02-26 17:01:20 -0300

    fix: protected routes resend

--- server/src/routes/messages.routes.js
+++ server/src/routes/messages.routes.js
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import { getMessages, replyMessage } from '../controllers/messages.controller.js';
 import isAdmin from '../middleware/isAdmin.js';
 
 const router = express.Router();
